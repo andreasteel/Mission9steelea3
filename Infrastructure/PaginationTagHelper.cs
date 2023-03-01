761930f512643425d76a72e51e66df7110c748d3
andreasteel <102710199+andreasteel@users.noreply.github.com>
2023-03-01 14:59:02 -0700

    Add project files.

---
+++ Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9steelea3.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-blah")]
+    public class PaginationTagHelper : TagHelper
+    {
+        //dynamically create page links 
+    }
+}
